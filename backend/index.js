7df1f548d0b4b257db0874c7f267e23a8a9ded8f
Pragya Gaur <gaur273pragya@gmail.com>
2024-12-26 16:06:01 +0530

    some changes

--- backend/index.js
+++ backend/index.js
@@ -31,10 +31,7 @@ const connectDb = async () => {
   if (mongoose.connection.readyState >= 1) {
     return;
   }
-  await mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+  await mongoose.connect(process.env.MONGO_URI);
 };
 
 app.use(async (req, res, next) => {

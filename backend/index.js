afca7a8e1a5c04df475877dbbd752d40b4459389
Pragya Gaur <gaur273pragya@gmail.com>
2024-12-26 16:13:33 +0530

    some changes

--- backend/index.js
+++ backend/index.js
@@ -147,5 +147,5 @@ app.get("/api/emails", verifyToken, async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.FRONTEND_URL || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

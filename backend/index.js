99f6330692e1f84ad09603b7d3b465566145cfea
Pragya Gaur <gaur273pragya@gmail.com>
2024-12-26 19:17:07 +0530

    some changes

--- backend/index.js
+++ backend/index.js
@@ -26,13 +26,23 @@ const verifyToken = require("./middleware/auth");
 app.use("/api", userRoutes);
 app.use("/api", mailRoutes);
 
-// Connect to MongoDB
+let isConnected;
+
 const connectDb = async () => {
-  if (mongoose.connection.readyState >= 1) {
+  if (isConnected) {
+    console.log("Using existing database connection");
     return;
   }
-  await mongoose.connect(process.env.MONGO_URI);
+
+  console.log("Creating new database connection");
+  const db = await mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  isConnected = db.connections[0].readyState;
 };
+
+
 
 app.use(async (req, res, next) => {
   await connectDb();
